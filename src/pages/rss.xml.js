47dab8361d532a1a4600606979164fc0698393e5
Iko <iko.jdr@gmail.com>
2025-11-25 17:01:11 +0100

    site: CONFIG.URL, in rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -13,7 +13,7 @@ export async function GET(context) {
         description: CONFIG.DESCRIPTION,
         // Pull in your project "site" from the endpoint context
         // https://docs.astro.build/en/reference/api-reference/#site
-        site: context.site,
+        site: CONFIG.URL,
         // Array of `<item>`s in output xml
         // See "Generating items" section for examples using content collections and glob imports
         items: blog.map((post) => ({
